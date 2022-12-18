4a0adfdeffa092ef5aacaffe02369f602d1e98b2
Ashutosh <106524453+Ashutosh741@users.noreply.github.com>
2022-12-18 19:34:03 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function footer() {
     return (
         <div className={classes.footer}>
             <div className={classes.center}>
-                <p>Made with ❤️ by <a href="https://github.com/omkar342" target="_blank">Omkar</a> <img src={git} alt="" /></p>
+                <p>Made with ❤️ by <a href="https://github.com/Ashutosh741" target="_blank">Ashutosh</a> <img src={git} alt="" /></p>
                 <p>Copyright © 2021</p>
             </div>
         </div>
